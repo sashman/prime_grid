c874ef937f97795113d6d4c810e81eb9ae687703
Sash <sashman90@gmail.com>
2016-03-09 14:10:49 +0000

    Additional unit test for a further prime numbers, spec name fix

--- spec/unit/prime_integer_spec.rb
+++ spec/unit/prime_integer_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe 'PrimeGeneration' do
 
   let(:subject) { (Class.new { include PrimeGrid::Integer::PrimeGeneration }).new }
 
-  it 'should generate 2 as first prime a number' do
+  it 'should generate 2 as a first prime number' do
 
     expect(subject.next_prime).to eq 2
 
   end
+
+  it 'should generate 2, 3, 5 as first three prime numbers' do
+
+    expect(subject.next_prime).to eq 2
+    expect(subject.next_prime).to eq 3
+    expect(subject.next_prime).to eq 5
+
+  end
+
 end
