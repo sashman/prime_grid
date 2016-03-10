db05ea3e43f0f92fc147bc9b54a8b0e1dff0c395
Sash <sashman90@gmail.com>
2016-03-10 19:25:57 +0000

    Implementing error handling for input header values

--- lib/prime_grid/grid_data/grid_model.rb
+++ lib/prime_grid/grid_data/grid_model.rb
@@ -8,6 +8,8 @@ module PrimeGrid
 
       def self.create_product_grid header_values
 
+        raise ArgumentError if header_values.nil? || (!header_values.is_a? Array) || header_values.empty?
+
         product_data = Array.new(header_values.count) { |y| Array.new(header_values.count) {|x|
           header_values[x] * header_values[y]
         } }
