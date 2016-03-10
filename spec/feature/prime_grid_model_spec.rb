7c41626bc15184e9aea1355f922434986b7aa557
Sash <sashman90@gmail.com>
2016-03-10 20:20:23 +0000

    Prime grid acceptance test

---
+++ spec/feature/prime_grid_model_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require "spec_helper"
+require 'prime_grid'
+
+RSpec.describe 'Prime number grid' do
+
+  before(:each) do
+
+  end
+
+  it "Given a number of primes, create a grid model" do
+
+   
+
+  end
+
+
+end
