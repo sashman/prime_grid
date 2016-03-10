c6122cfba5046727f57578edc5183a3848b8f9f6
Sash <sashman90@gmail.com>
2016-03-10 18:31:20 +0000

    Passing basic test with initial implementation

--- lib/prime_grid/grid_data/grid_model.rb
+++ lib/prime_grid/grid_data/grid_model.rb
@@ -2,8 +2,22 @@ module PrimeGrid
   module GridData
     class GridModel
 
+      def initialize grid_data
+        @grid_data = grid_data
+      end
+
       def self.create_product_grid header_values
 
+        product_data = Array.new(header_values.count) { |y| Array.new(header_values.count) {
+            header_values[0] * header_values[0]
+        } }
+
+        GridModel.new product_data
+
+      end
+
+      def value_at x, y
+        @grid_data[y][x]
       end
 
     end
