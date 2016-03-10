0d700b5993cd3cc7403f4c4b9e1e3feb695ed641
Sash <sashman90@gmail.com>
2016-03-10 18:53:05 +0000

    Adding a more advanced test

--- spec/unit/grid_model_spec.rb
+++ spec/unit/grid_model_spec.rb
@@ -8,10 +8,29 @@ describe 'GridModel' do
 
     context 'given column/row header values as an array of integers' do
 
-      it 'should create an instance of GridModel with products as the cell values' do
+      it 'should create an instance of GridModel with products as the cell values for array of size one' do
 
         subject = PrimeGrid::GridData::GridModel.create_product_grid [4]
         expect(subject.value_at 0, 0).to eq 16
+
+      end
+
+      it 'should create an instance of GridModel with products as the cell values for array with multiple elements' do
+
+        subject = PrimeGrid::GridData::GridModel.create_product_grid [4, 1, 8]
+
+        [
+            [0, 0, 16],
+            [1, 1, 1],
+            [2, 2, 64],
+
+            [0, 1, 4],
+            [0, 2, 32],
+
+            [1, 2, 8],
+        ].each do | x, y, expected |
+          expect(subject.value_at x, y).to eq expected
+        end
 
       end
 
