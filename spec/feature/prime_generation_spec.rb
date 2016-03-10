e2869a94ab2ff2860dde851b66d9d55b2729c96c
Sash <sashman90@gmail.com>
2016-03-10 16:13:12 +0000

    Passing basic acceptance test

--- spec/feature/prime_generation_spec.rb
+++ spec/feature/prime_generation_spec.rb
@@ -3,9 +3,23 @@ require "spec_helper"
 
 RSpec.describe 'Prime number generation' do
 
+  before(:each) do
+
+    class Integer
+
+      include PrimeGrid::Integer::PrimeGeneration
+
+      def primes
+        take_primes self
+      end
+
+    end
+
+  end
+
   it "should generate 1 prime number" do
 
-    expect(1.primes).to eq 2
+    expect(1.primes).to eq [2]
 
   end
 
