5bc5b830937615f5c9006287adfa55d833138acb
Sash <sashman90@gmail.com>
2016-03-10 23:00:43 +0000

    Adding requires

--- lib/prime_grid.rb
+++ lib/prime_grid.rb
@@ -1,6 +1,10 @@
 require "prime_grid/version"
 require "prime_grid/integer/integer_prime_generation"
+require "prime_grid/grid_data/grid_model"
+require "prime_grid/controllers/prime_grid_controller"
 
 module PrimeGrid
+
+
 
 end
