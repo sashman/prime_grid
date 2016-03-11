3994b57500593ef4a35c5ebccba94bc332af35dc
Sash <sashman90@gmail.com>
2016-03-11 21:27:26 +0000

    Fixing expectations

--- spec/unit/grid_model_helper_spec.rb
+++ spec/unit/grid_model_helper_spec.rb
@@ -10,8 +10,8 @@ describe 'GridModelHelper' do
     it 'should return the string length of the bottom left cell value' do
 
       model = double(PrimeGrid::GridData::GridModel)
-      expect(model).to receive(:column_values)
-      expect(model).to receive(:row_values)
+      expect(model).to receive(:column_values).and_return([])
+      expect(model).to receive(:row_values).and_return([])
       expect(model).to receive(:value_at).and_return(1234)
 
       expect(PrimeGrid::Utils::GridModelHelper.max_prime_grid_padding model).to eq 4
