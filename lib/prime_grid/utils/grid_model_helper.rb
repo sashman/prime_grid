5349e1afc58f92369114946c3e4655936557c0a7
Sash <sashman90@gmail.com>
2016-03-11 21:28:08 +0000

    Implementing padding helper method

--- lib/prime_grid/utils/grid_model_helper.rb
+++ lib/prime_grid/utils/grid_model_helper.rb
@@ -3,7 +3,9 @@ module PrimeGrid
     class GridModelHelper
 
       def self.max_prime_grid_padding grid_model
-
+        max_x_index = grid_model.column_values.count
+        max_y_index = grid_model.row_values.count
+        grid_model.value_at(max_x_index, max_y_index).to_s.size
       end
 
     end
